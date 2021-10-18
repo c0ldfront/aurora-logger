3c9b242e48d98fa370471abf87c1b1aad459fdeb
David Charles Mydlarz <coldfront@gmail.com>
2021-10-18 23:56:06 +0900

    Setup Package

--- src/logger.interface.ts
+++ src/logger.interface.ts
@@ -1,11 +1,11 @@
 interface ILogger {
-    log(message: string): void;
-    trace(tag: string, message: string, ...args: any[]): void
-    debug(tag: string, message: string, ...args: any[]): void
-    info(tag: string, message: string, ...args: any[]): void
-    warn(tag: string, message: string, ...args: any[]): void
-    error(tag: string, message: string, ...args: any[]): void
-    fatal(tag: string, message: string, ...args: any[]): void
+  log(message: string): void;
+  trace(tag: string, message: string, ...args: any[]): void;
+  debug(tag: string, message: string, ...args: any[]): void;
+  info(tag: string, message: string, ...args: any[]): void;
+  warn(tag: string, message: string, ...args: any[]): void;
+  error(tag: string, message: string, ...args: any[]): void;
+  fatal(tag: string, message: string, ...args: any[]): void;
 }
 
 export default ILogger;
